32ee48f4bd4fad5ae890f654f11842e25c578c36
abir <abir196600@gmail.com>
2022-10-14 02:35:02 +0600

    Enter test report activity changes

--- app/src/main/java/com/example/niramoy/EnterTestReportActivity.java
+++ app/src/main/java/com/example/niramoy/EnterTestReportActivity.java
@@ -3,12 +3,31 @@ package com.example.niramoy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class EnterTestReportActivity extends AppCompatActivity {
+
+    String[] tests = {"ECG","RA","Glucose level","Blood","etc"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> arrayAdapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_test_report);
+
+        arrayAdapterItems = new ArrayAdapter<String>(this,R.layout.test_list,tests);
+        autoCompleteTextView.setAdapter(arrayAdapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String test = parent.getItemAtPosition(position).toString();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/niramoy/SplashScreenActivity.java
+++ app/src/main/java/com/example/niramoy/SplashScreenActivity.java
@@ -2,13 +2,26 @@ package com.example.niramoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    //Button button = findViewById(R.id.button);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        /*button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this,EnterTestReportActivity.class));
+            }
+        });*/
     }
 }
