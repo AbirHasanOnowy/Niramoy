d0a9754a8274dd10681e84f2f4f7086913d216d9
Rafi Al Mahmud <rafialmahmudc@gmail.com>
2022-10-13 18:19:50 +0600

    activities

---
+++ app/src/main/java/com/example/niramoy/PatientProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.niramoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_profile);
+    }
+}
