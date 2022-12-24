1185aef412098b0bf852085f59d3d7edd127ba0a
Rafi Al Mahmud <rafialmahmudc@gmail.com>
2022-12-24 13:43:00 +0600

    nav bar

--- app/src/main/java/com/example/niramoy/AdminMainActivity.java
+++ app/src/main/java/com/example/niramoy/AdminMainActivity.java
@@ -2,13 +2,25 @@ package com.example.niramoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AdminMainActivity extends AppCompatActivity {
-
+    FloatingActionButton addHospitalButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+        addHospitalButton=findViewById(R.id.addHospitalButton);
+        addHospitalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminMainActivity.this,AddHospitalActivityAdmin.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/niramoy/MainActivity.java
+++ app/src/main/java/com/example/niramoy/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.niramoy;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    ImageView imageMenu;
     FloatingActionButton addButton, addTestButton, addPatientButton,addPrescriptionButton;
     Boolean isVis;
     @Override
@@ -23,6 +34,50 @@ public class MainActivity extends AppCompatActivity {
         addTestButton = findViewById(R.id.addTestButton);
         addPatientButton = findViewById(R.id.addPatientButton);
         addPrescriptionButton=findViewById(R.id.addPrescriptionButton);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_View);
+        imageMenu = findViewById(R.id.imageMenu);
+
+        toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.mHome:
+                        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.mShare:
+                        Toast.makeText(MainActivity.this, "Facebook", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+        //------------------------------
+        // ------------------------
+        // App Bar Click Event
+        imageMenu = findViewById(R.id.imageMenu);
+
+        imageMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code Here
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+
         addPrescriptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         addPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,EnterPatientActivity.class));
+                startActivity(new Intent(MainActivity.this, EnterPatientActivity.class));
             }
         });
 
@@ -74,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
         addPrescriptionButton.show();
         Drawable drawable = addButton.getBackground();
         drawable=DrawableCompat.wrap(drawable);
-        DrawableCompat.setTint(drawable,ContextCompat.getColor(this,R.color.black));
+        DrawableCompat.setTint(drawable,ContextCompat.getColor(this,R.color.gray));
         addButton.setBackground(drawable);
         addButton.setImageResource(R.drawable.ic_baseline_cancel_24);
        // addButton.setBackgroundColor(ContextCompat.getColor(this,R.color.black));
