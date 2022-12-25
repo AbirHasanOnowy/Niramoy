7f5303b06aca08283f977fb0a2517135ac129233
abir <abir196600@gmail.com>
2022-12-25 18:34:38 +0600

    SignIn and SignUp action complete with few navigation bar data changes

--- app/src/main/java/com/example/niramoy/SignInActivity.java
+++ app/src/main/java/com/example/niramoy/SignInActivity.java
@@ -1,24 +1,47 @@
 package com.example.niramoy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
 
-    EditText ePassword,eEmail;
-    TextInputLayout layoutEmail,layoutPassword;
-    MaterialButton logIn,createId;
-    String dMail="abir196600@gmail.com",dPassword="jkl",iMail,iPassword;
+    EditText ePassword, eEmail;
+    TextInputLayout layoutEmail, layoutPassword;
+    MaterialButton logIn, createId;
+    String email, password, uid, status, position, name;
+
+    private FirebaseAuth fAuth;
+    private FirebaseFirestore fStore;
+    private DocumentReference uidRef;
+
+    private static final String KEY_POS = "Position";
+    private static final String KEY_NAME = "Name";
+    private static final String KEY_VERIFY = "Verified";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,41 +55,91 @@ public class SignInActivity extends AppCompatActivity {
         logIn = findViewById(R.id.logInButton);
         createId = findViewById(R.id.createIdButton);
 
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(eEmail.length() != 0)  {
-                    if (ePassword.length() != 0){
+                email = eEmail.getText().toString();
+                password = ePassword.getText().toString();
 
-                        iMail = eEmail.getText().toString();
-                        iPassword = ePassword.getText().toString();
-
-                        if(Objects.equals(iMail, dMail) && iPassword.equals(dPassword)) {
-                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                            finish();
-                        } else {
-                            layoutPassword.setError("Enter Correct Password");
-                        }
-                    } else {
-                        layoutPassword.setError("Enter Valid Password");
-                    }
-                } else {
-                    layoutEmail.setError("Enter Valid Email");
+                if (email.isEmpty()) {
+                    eEmail.setError("Email is required");
+                    eEmail.requestFocus();
+                    return;
                 }
 
-            }
-        });
+                if (password.isEmpty()) {
+                    ePassword.setError("Password is required");
+                    ePassword.requestFocus();
+                    return;
+                }
 
-        createId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(SignInActivity.this, SignUp.class));
-                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    eEmail.setError("Please provide valid email");
+                    eEmail.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    ePassword.setError("Password length min 6");
+                    ePassword.requestFocus();
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            uid = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
+                            uidRef = fStore.collection("UID").document(uid);
+                            uidRef.addSnapshotListener(SignInActivity.this, new EventListener<DocumentSnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                                    status = value.getString(KEY_VERIFY);
+                                    name = value.getString(KEY_NAME);
+                                    position = value.getString(KEY_POS);
+
+                                    if (Objects.equals(status, "Yes")) {
+                                        if (Objects.equals(position, "Doctor") || Objects.equals(position, "Receptionist")) {
+                                            Toast.makeText(SignInActivity.this, "Welcome " + name, Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                                        } else if (Objects.equals(position, "Director")) {
+                                            Toast.makeText(SignInActivity.this, "Welcome " + name, Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(SignInActivity.this, DirectorMainActivity.class));
+                                        } else if (Objects.equals(position, "Admin")) {
+                                            Toast.makeText(SignInActivity.this, "Welcome " + name, Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(SignInActivity.this, AdminMainActivity.class));
+                                        } else {
+                                            Toast.makeText(SignInActivity.this, "Welcome " + name, Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(SignInActivity.this, NurseMainActivity.class));
+                                        }
+                                    } else {
+                                        Toast.makeText(SignInActivity.this, "Please wait for Director to assign you", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Invalid Email or Password", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+                createId.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //startActivity(new Intent(SignInActivity.this, SignUp.class));
+                        startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+
+                    }
+                });
 
             }
         });
 
     }
-
 }
--- app/src/main/java/com/example/niramoy/SignUpActivity.java
+++ app/src/main/java/com/example/niramoy/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.niramoy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.Objects;
 
@@ -17,14 +26,26 @@ public class SignUpActivity extends AppCompatActivity {
 
     MaterialButton nextButton;
     String[] positionList = {"Director","Doctor","Receptionist","Nurse"};
+    TextInputLayout layoutHID,layoutPosition;
+    EditText eHID;
     AutoCompleteTextView positionAutoCompleteTextView;
     ArrayAdapter<String> positionArrayAdapterItems;
-    String Position="";
+    String Position="",HID;
+
+    private FirebaseFirestore fstore;
+    private DocumentReference dbreference;
+    private static final String KEY_ASSIGN = "Assigned";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        fstore = FirebaseFirestore.getInstance();
+
+        eHID = findViewById(R.id.hidHospitalIdInput);
+        layoutHID = findViewById(R.id.hidInputField);
+        layoutPosition = findViewById(R.id.hidPositionMenu);
 
         positionAutoCompleteTextView = findViewById(R.id.hidPositionSelectDropDownBox);
         nextButton=findViewById(R.id.hidNextButton);
@@ -42,15 +63,63 @@ public class SignUpActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(Objects.equals(Position, "Director")){
-                    startActivity(new Intent(SignUpActivity.this, SignUpDirectorActivity.class));
-                } else if(Objects.equals(Position, "Doctor")){
-                    startActivity(new Intent(SignUpActivity.this, SignUpDoctorActivity.class));
-                }else if(Objects.equals(Position, "Receptionist")){
-                    startActivity(new Intent(SignUpActivity.this, SignUpReceptionistActivity.class));
-                } else if(Objects.equals(Position, "Nurse")){
-                    startActivity(new Intent(SignUpActivity.this, SignUpNurseActivity.class));
+                HID = eHID.getText().toString().trim();
+
+                if(HID.isEmpty())
+                {
+                    layoutHID.setError("Hospital ID is required");
+                    layoutHID.requestFocus();
+                    return;
                 }
+
+                if(Objects.equals(Position, ""))
+                {
+                    layoutPosition.setError("Email is required");
+                    layoutPosition.requestFocus();
+                    return;
+                }
+
+                if(Objects.equals(Position, "Director"))
+                {
+                    Intent intent = new Intent(SignUpActivity.this,SignUpDoctorActivity.class);
+                    intent.putExtra("HID",HID);
+                    intent.putExtra("Position",Position);
+                    startActivity(intent);
+                    return;
+                }
+
+                dbreference = fstore.collection("Hospitals").document(HID);
+                dbreference.addSnapshotListener(SignUpActivity.this,new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        String val = value.getString(KEY_ASSIGN);
+
+                        if(Objects.equals(val, "Yes")) {
+                            Intent intent = new Intent(SignUpActivity.this,SignUpDoctorActivity.class);
+                            intent.putExtra("HID",HID);
+                            intent.putExtra("Position",Position);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignUpActivity.this,"Invalid Hospital ID",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+//                if(Objects.equals(Position, "Director")){
+//                    startActivity(new Intent(SignUpActivity.this, SignUpDirectorActivity.class));
+//                } else if(Objects.equals(Position, "Doctor")){
+//                    startActivity(new Intent(SignUpActivity.this, SignUpDoctorActivity.class));
+//                }else if(Objects.equals(Position, "Receptionist")){
+//                    startActivity(new Intent(SignUpActivity.this, SignUpReceptionistActivity.class));
+//                } else if(Objects.equals(Position, "Nurse")){
+//                    startActivity(new Intent(SignUpActivity.this, SignUpNurseActivity.class));
+//                }
             }
         });
     }
